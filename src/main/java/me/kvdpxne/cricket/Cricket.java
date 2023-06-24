b9f11c3bcbc71d7dd536fb01c48fa80903c3b711
kvdpxne <kvdpxne@gmail.com>
2023-06-25 01:57:15 +0200

    Added getter for logger name & the possibility of logging exception messages.

--- src/main/java/me/kvdpxne/cricket/Cricket.java
+++ src/main/java/me/kvdpxne/cricket/Cricket.java
@@ -11,12 +11,31 @@ public class Cricket {
     this.logger = logger;
   }
 
+  /**
+   * The current name of the logger that this object uses to print messages,
+   * may be null if an anonymous logger was used to create a new instance of
+   * this object.
+   *
+   * @return The logger name or null.
+   */
+  public String getName() {
+    return this.logger.getName();
+  }
+
+  public void error(final String message, final Throwable throwable) {
+    this.logger.log(Level.SEVERE, message, throwable);
+  }
+
   public void error(final String message, final Object... arguments) {
     this.logger.log(Level.SEVERE, message, arguments);
   }
 
   public void error(final String message) {
     this.logger.log(Level.SEVERE, message);
+  }
+
+  public void warn(final String message, final Throwable throwable) {
+    this.logger.log(Level.WARNING, message, throwable);
   }
 
   public void warn(final String message, final Object... arguments) {
@@ -27,12 +46,20 @@ public class Cricket {
     this.logger.log(Level.WARNING, message);
   }
 
+  public void info(final String message, final Throwable throwable) {
+    this.logger.log(Level.INFO, message, throwable);
+  }
+
   public void info(final String message, final Object... arguments) {
     this.logger.log(Level.INFO, message, arguments);
   }
 
   public void info(final String message) {
     this.logger.log(Level.INFO, message);
+  }
+
+  public void debug(final String message, final Throwable throwable) {
+    this.logger.log(Level.CONFIG, message, throwable);
   }
 
   public void debug(final String message, final Object... arguments) {
